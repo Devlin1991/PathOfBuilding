68d08b38913dffb3bce2eb74c98b4fb121341617
Paliak <91493239+Paliak@users.noreply.github.com>
2024-03-25 22:58:20 +0100

    FIX: remove vestigial exclude list

--- src/Data/Global.lua
+++ src/Data/Global.lua
@@ -317,7 +317,6 @@ SkillType = {
 
 GlobalCache = { 
 	cachedData = { MAIN = {}, CALCS = {}, CALCULATOR = {}, CACHE = {}, },
-	excludeFullDpsList = { },
 	noCache = nil,
 	useFullDPS = false,
 	numActiveSkillInFullDPS = 0,
